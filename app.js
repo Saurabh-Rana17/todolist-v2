c69f57b14ee184310ac05578399538dd28385d7e
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-08 23:10:28 +0530

    mogoose connected and default entries added

--- app.js
+++ app.js
@@ -2,28 +2,49 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+// async function main() {
+const itemsSchema = {
+  name: String,
+};
+const Item = mongoose.model("Item ", itemsSchema);
+const item1 = new Item({
+  name: "welcome",
+});
+const item2 = new Item({
+  name: "how are you",
+});
+const item3 = new Item({
+  name: "this is 3rd obj",
+});
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems).then(() => {
+//   console.log("data inserted");
+// });
+// }
+
 const workItems = [];
 
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+app.get("/", function (req, res) {
+  run();
+  async function run() {
+    let foundItem = await Item.find({});
+    console.log(foundItem);
+    res.render("list", { listTitle: "Today", newListItems: foundItem });
+  }
 });
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
   const item = req.body.newItem;
 
   if (req.body.list === "Work") {
@@ -35,14 +56,14 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
